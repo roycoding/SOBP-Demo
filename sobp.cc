98c45b7ab44eddd0a9cd58ebcb13021b34141394
Roy Keyes <roy.coding@gmail.com>
2011-06-15 02:20:51 -0600

    Removed unused random seed code.

--- sobp.cc
+++ sobp.cc
@@ -14,34 +14,9 @@
 #include "G4UIExecutive.hh"
 #endif
 
-using namespace CLHEP;
-#include <time.h>
-
 
 int main(int argc,char** argv)
 {
-//    // Random seed code
-//	CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
-//	G4int seed = time(0);
-//	G4cout<<"Time is: "<<seed<<G4endl;
-
-//    // Seeding the random number generator: can be done by say getting a 2nd argc value
-//	if ((argc == 2)||(argc == 3))
-//	{
-//		G4cout<<"Single input detected, seed will  from this..."<<G4endl;
-//	
-//		string seed = argv[1];	// at this stage the seed is a string! -> convert to char then to an integer!
-//		
-//		const char *new_seed;
-//		new_seed = seed.c_str();	// method to convert a string to char
-//				
-//		int seed2;
-//		seed2 = atoi(new_seed);		// function to convert a char to an int
-
-//		CLHEP::HepRandom :: setTheSeed(seed2);
-//		G4cout<<"The seed has been set to: "<< seed2 <<G4endl;
-
-//	}
 
   // Run manager
   G4RunManager * runManager = new G4RunManager;
